961713105f95b31b49fb45dad75839e525638309
JaceRider <jacerider@gmail.com>
2025-05-29 16:22:46 -0400

    Use jquery for firing event so additional params are passed.

--- exo_modal/src/ts/exo.modal/_exo.modal.ts
+++ exo_modal/src/ts/exo.modal/_exo.modal.ts
@@ -1350,7 +1350,7 @@ class ExoModal extends ExoData {
     }
 
     this.event('closed').trigger(this);
-    window.dispatchEvent(new Event('dialog:afterclose'));
+    $(window).trigger('dialog:afterclose', [{}, this.$element]);
     this.callCallback('onClosed');
     this.$element.removeClass('isOpen');
 
